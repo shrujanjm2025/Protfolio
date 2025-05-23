706cf36d72403677af88a57e269030c1aa6c9128
Shrujan J M <shrujanjm@gmail.com>
2025-05-23 15:59:30 +0530

    Updated Commit1

--- PortfolioScript.js
+++ PortfolioScript.js
@@ -15,3 +15,32 @@ let slideIndex = 1;
             }
             slides[slideIndex - 1].style.display = "block";
         }
+        function scrollToProfile() {
+            document.getElementById("header").scrollIntoView({ behavior: "smooth" });
+    }
+        function scrollToAboutme() {
+            document.getElementById("aboutme").scrollIntoView({ behavior: "smooth" });
+    }
+        function scrollToMyskills() {
+            document.getElementById("myskills").scrollIntoView({ behavior: "smooth" });
+    }
+        function scrollToContactinfo() {
+            document.getElementById("contactinfo").scrollIntoView({ behavior: "smooth" });
+    }
+        function animateSkills() {
+    const skillSection = document.getElementById('myskills');
+    const skillBars = document.querySelectorAll('.skillrate');
+
+    const sectionPos = skillSection.getBoundingClientRect().top;
+    const screenPos = window.innerHeight;
+
+    if (sectionPos < screenPos - 100) {
+      skillBars.forEach(bar => {
+        bar.style.width = bar.getAttribute('data-width');
+      });
+      window.removeEventListener('scroll', animateSkills);
+    }
+  }
+
+  window.addEventListener('scroll', animateSkills);
+  window.addEventListener('load', animateSkills);
